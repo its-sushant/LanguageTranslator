9e0813697d8337889f3b9763c4451896e3de100f
Sushant Kumar <sushantmishra02102002@gmail.com>
2023-10-21 14:33:48 +0530

    Refactor javascript code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+from decouple import config
 import requests
 
 app = Flask(__name__)
@@ -13,7 +14,8 @@ def translate():
     source_lang = request.form.get('sourceLang')
     target_lang = request.form.get('targetLang')
 
-    api_url = 'https://translate.google.com/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&iid=1dd3b944-fa62-4b55-b330-74909a99969e'
+    api_url = config('API_URL')
+
     data = {
         'sl': source_lang,
         'tl': target_lang,
@@ -23,7 +25,7 @@ def translate():
     response = requests.post(api_url, data=data)
     translation_data = response.json()
     translation = translation_data['sentences'][0]['trans']
-
+    
     return jsonify({'translation': translation})
 
 if __name__ == '__main__':
